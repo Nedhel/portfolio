c87c82935dfdf01f91aa63827b5e6d172bea0e70
Carlos Zalpa <nedhel.90@gmail.com>
2022-11-08 16:57:12 -0600

    Adding NavBar and styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
     content: ["./src/**/*.{js,jsx,ts,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: "#292C33",
+                secondary: "#212329",
+                terciary: "#00ccff",
+            },
+        },
     },
     plugins: [],
 };
