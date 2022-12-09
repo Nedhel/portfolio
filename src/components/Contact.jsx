286a36bfeb6762949a0f88f338bd23efb909dec8
Carlos Zalpa <nedhel.90@gmail.com>
2022-12-09 15:32:02 -0600

    Contact file modify

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,24 @@
+import {HiOutlineChip} from "react-icons/hi";
+import { SiGmail, SiGithub, SiLinkedin } from "react-icons/si";
 
-import { Card } from "./Card";
 export function Contact(){    
     return(
-        <div className="flex flex-col items-center h-96 justify-around">
-          <h1 className="font-semibold text-xl">Contacto</h1>
-            <Card />
+        <div className="flex flex-col items-center h-screen justify-evenly">
+            <h1 className='font-bold text-2xl text-center'><HiOutlineChip className='inline text-terciary'/> Contacto</h1>         
+            
+                <p>Gracias por interesarte en ponerte en contacto comigo sera un placer trabajar juntos
+                    te dejo mis medios de contacto.
+                </p>
+                <div className="grid grid-cols-2 justify-items-center gap-8">
+                    <div className="col-span-2">
+                        <a href="https://www.linkedin.com/in/carlos-nedhel-zalpa-garcia-a8067322a/" target='_blank' rel='noreferrer'><SiLinkedin className="inline text-4xl"/> linkedin.com/in/carlos-nedhel-zalpa-garcia-a8067322a</a>
+                    </div>
+                    <div className=""><SiGmail className="inline text-4xl"/> nedhel.90@gmail.com</div>
+                    <div className="">
+                        <a href='https://github.com/Nedhel' target='_blank' rel='noreferrer'><SiGithub className="inline text-4xl"/> github.com/Nedhel</a>
+                    </div>
+                </div>
+            
         </div>
     )
 }
