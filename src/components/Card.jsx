38c1fd3d11be0b661cdefc363d81f9d99043910b
Carlos Zalpa <nedhel.90@gmail.com>
2022-12-22 17:16:58 -0600

    Cars project finish

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,6 @@
 
 import { useState } from 'react'
-import { BsFillArrowRightCircleFill, BsFillArrowLeftCircleFill } from "react-icons/bs";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 
 export function Card({name, description, photos}){
     const [number, setNumber]=useState(0)
@@ -14,16 +14,16 @@ export function Card({name, description, photos}){
         console.log(number)
     }
     return (
-        <div className="border flex flex-col w-96 h-80 justify-evenly items-center relative bg-secondary">
-            <div className="border relative">
+        <div className="flex flex-col w-96 h-80 justify-evenly items-center relative bg-primary shadow-md shadow-white">
+            <div className="border relative mask">
                 <img src={photos[number]} alt="" className='w-full'/>
-                <button onClick={prevImage} className='text-primary text-xl absolute top-1/2 left-0' disabled={number===0?true:false}><BsFillArrowLeftCircleFill /></button> 
-                <button onClick={nextImage} className='text-primary text-xl absolute top-1/2 right-0' disabled={number===photos.length-1?true:false}><BsFillArrowRightCircleFill /></button> 
             </div>
-            <div className="border">
+            <button onClick={prevImage} className='text-white text-2xl absolute hidden arrow top-1/4 left-0' disabled={number===0?true:false}><IoIosArrowBack /></button> 
+            <button onClick={nextImage} className='text-white text-2xl absolute hidden arrow top-1/4 right-0' disabled={number===photos.length-1?true:false}><IoIosArrowForward /></button> 
+            <div>
                 {name}                
             </div>
-            <div className='dropdown-description'>{description}</div>
+            <div className='dropdown-description text-sm'>{description}</div>
         </div>
     )
 }
